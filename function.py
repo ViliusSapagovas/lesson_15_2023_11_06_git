a6f944b55575fd57912de0fa48ef3bcc63b17026
Vilius Sapagovas <vilius.sapagovas2000@gmail.com>
2023-11-06 19:04:54 +0100

    fixed

--- function.py
+++ function.py
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 def add_numbers(a: int, c: int) -> int:
     return a + c
-=======
-def add_digits(a: int, b: int) -> int:
+
+print(add_numbers(10,5))
+
+def add_digits(a, b):
     return a + b
->>>>>>> f4747b6618df94048e87ebbda318882921e37a74
 
 print(add_digits(10,5))
-
-def add_numbers(a, b):
-    return a + b
